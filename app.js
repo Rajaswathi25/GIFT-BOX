e891b79ffcc9db27edcb9d056f72b6e5c6cd47af
Rajaswathi G <727721euec119@skcet.ac.in>
2022-07-09 21:48:52 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const but = document.getElementById("generatebutton");
+
+const grid = document.getElementById("grid");
+
+but.onclick =() =>
+
+{
+
+ const noofgb = document.getElementById("textBox").value;
+
+ console.log(noofgb);
+
+ let i;
+
+ for(i=1;i<=noofgb;i++){
+
+ const gb = document.createElement("div");
+
+ grid.append(gb);
+
+ gb.innerHTML = `GIFT`;
+
+ gb.style.height = `5vh`;
+
+ gb.style.textAlign = `center`;
+ gb.style.paddingTop=`20px`;
+ gb.style.width = `5vw`;
+ gb.style.margin = ` 20px 5px`;
+ gb.style.border = `3px solid black`;
+
+}
+
+}
